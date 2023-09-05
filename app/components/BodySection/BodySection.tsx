1e8e865c237779cb2f9bc94c29a5901a2221b0e9
leo-mutuku <le.mutuku@gmail.com>
2023-09-05 20:09:54 +0300

    Fixed p tag error

--- app/components/BodySection/BodySection.tsx
+++ app/components/BodySection/BodySection.tsx
@@ -6,7 +6,7 @@ const BodySection = () => {
         <div className='rounded-sm px-6 w-[75%] items-start justify-between' style={{"border":"solid 1px gray", "borderRadius":"10px" 
     }}>
             <div className='py-6  underline'><span className='text-underline'>About me </span></div>
-            <p className=''>Leo Mutuku is a results-driven Developer with a passion for leveraging innovative strategies to drive brand growth and customer engagement in the ever-evolving digital landscape.</p>
+            <p className=''>Leonard Mutuku is a results-driven Developer with a passion for leveraging innovative strategies to drive brand growth and customer engagement in the ever-evolving digital landscape.</p>
 
         </div>
     </div>
