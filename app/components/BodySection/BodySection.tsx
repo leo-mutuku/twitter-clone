2ef75263a5f9559b115257cd02a697ae18deef9f
leo-mutuku <le.mutuku@gmail.com>
2023-09-05 20:03:38 +0300

    Fixed p tag error

--- app/components/BodySection/BodySection.tsx
+++ app/components/BodySection/BodySection.tsx
@@ -1,25 +1,17 @@
-import React from "react";
+import React from 'react'
 
 const BodySection = () => {
   return (
-    <div className="flex flex-col py-4 items-center justify-center gap-8 ">
-      <div
-        className="rounded-sm px-6 w-[75%] items-start justify-between"
-        style={{ border: "solid 1px gray", borderRadius: "10px" }}
-      >
-        <div className="py-6  underline">
-          <span className="text-underline">About me </span>
-        </div>
-        <p>
-          I have over 8years of experience in total. For the last 5years I've
-          worked as a full-time software developer on Nodejs, React, and Spring
-          Boot. I believe in getting the work done in the shortest time possible
-          and having my client review it. I advise my client on what to do if no
-          plan provided. The aim is to reduce cost and deliver quality product.
-        </p>
-      </div>
-    </div>
-  );
-};
+    <div className='flex flex-col py-4 items-center justify-center gap-8 '>
+        <div className='rounded-sm px-6 w-[75%] items-start justify-between' style={{"border":"solid 1px gray", "borderRadius":"10px" 
+    }}>
+            <div className='py-6  underline'><span className='text-underline'>About me </span></div>
+            <p className=''>Leo Mutuku is a results-driven Developer with a passion for leveraging innovative strategies to drive brand growth and customer engagement in the ever-evolving digital landscape.</p>
 
-export default BodySection;
+        </div>
+    </div>
+
+  )
+}
+
+export default BodySection
