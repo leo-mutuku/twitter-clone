63a5a5a097917d0bf21d9156c6246b9ca913b055
leo-mutuku <le.mutuku@gmail.com>
2023-09-05 20:34:54 +0300

    Fixed p tag error

--- app/components/BodySection/PersonalProfile.tsx
+++ app/components/BodySection/PersonalProfile.tsx
@@ -7,7 +7,8 @@ const PersonalProfile = () => {
     }}>
             <div className='py-6  underline'><span className='text-underline'>About me </span></div>
             <p className=''>Leonard Mutuku is a results-driven Developer with a passion for leveraging innovative strategies to drive brand growth and customer engagement in the ever-evolving digital landscape.</p>
-
+            <p>I have over 8years of experience in total. For the last 5years I've worked as a full-time software developer on Nodejs, React, and Spring Boot.</p>
+            <p>I believe in getting the work done in the shortest time possible and having my client review it. I advise my client on what to do if no plan provided. The aim is to reduce cost and deliver quality produc</p>
         </div>
     </div>
 
